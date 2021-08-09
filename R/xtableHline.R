f455232f7545475076e6cc126865fd9f145f9f0f
John L. Godlee <johngodlee@gmail.com>
2021-08-09 16:00:18 +0100

    Added function for xtables

---
+++ R/xtableHline.R
@@ -0,0 +1,31 @@
+#' Calculate vector of hline positions for xtable's hline.after parameter
+#'
+#' @param ngroup number of groups in the table
+#' @param nsize number of members in each group
+#'
+#' @return vector of hline positions 
+#' 
+#' @export
+#' 
+xtableHline <- function(ngroup, nsize) {
+  if (length(ngroup) > 1 | !is.numeric(ngroup)) {
+    stop("ngroup must be a numeric vector of length 1")
+  }
+  if (!is.numeric(nsize)) { 
+    stop("nsize must be a numeric vector")
+  }
+  if (length(nsize) > 1 & length(nsize) != ngroup) {
+    stop("if nsize is a vector its length must be identical to the value of ngroup")
+  }
+
+  if (length(nsize) == 1) {
+    out <- c(-1, 0, 
+      seq(from = nsize, by = nsize, length.out = ngroup -1), 
+      ngroup*nsize)
+  } else {
+    out <- c(-1, 0, cumsum(nsize))
+  }
+
+  return(out)
+}
+
