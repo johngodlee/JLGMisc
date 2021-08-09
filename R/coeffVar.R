1cd27b8586d6f8d16f4cbb4f21f415c8549d4a69
John L. Godlee <johngodlee@gmail.com>
2021-08-09 20:57:03 +0100

    Added function

---
+++ R/coeffVar.R
@@ -0,0 +1,13 @@
+#' Calculate the coefficient of variation
+#'
+#' @param x numeric vector
+#'
+#' @return coefficient of variation of vector
+#' 
+#' @examples
+#' coeffVar(seq(0, 10))
+#' @export
+#' 
+coeffVar <- function(x) {
+  sd(x) / mean(x) * 100,
+}
