1a12774c052ea0fd6c8bc5683adfd3b433046d4b
John L. Godlee <johngodlee@gmail.com>
2022-12-21 21:26:41 +0000

    Added function to find hex triplets

---
+++ R/hexTrip.R
@@ -0,0 +1,22 @@
+#' Find the closest hex triplet to a given six character hex colour
+#'
+#' @param x vector of six digit hex colours
+#'
+#' @return 
+#' 
+#' @examples
+#' hexTrip(c("#117733", "#b58900", "#855C75"))
+#' 
+#' @export
+#' 
+hexTrip <- function(x) {
+  if (any(nchar(x) != 7) | any(!grepl("^#", x))) {
+    stop ("Hex code invalid")
+  }
+
+  unlist(lapply(x, function(y) {
+    paste(c("#", sprintf("%x", (col2rgb(y) + 8) %/% 17)), collapse = "")
+  }))
+}
+
+
